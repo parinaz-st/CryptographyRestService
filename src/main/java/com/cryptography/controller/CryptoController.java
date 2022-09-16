32035c7cae2a6e0c4263a64d8bba04bef2c66fa4
parinaz <p.sattarzadeh@gmail.com>
2022-09-16 23:56:11 +0430

    Add encrypt & decrypt methods in service

--- src/main/java/com/cryptography/controller/CryptoController.java
+++ src/main/java/com/cryptography/controller/CryptoController.java
@@ -1,15 +1,42 @@
 package com.cryptography.controller;
 
+import com.cryptography.dto.DecryptReqDto;
+import com.cryptography.dto.EncryptReqDto;
+import com.cryptography.service.CryptoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CryptoController {
+
+    @Autowired
+    CryptoService cryptoService;
     @GetMapping("/test")
     public ResponseEntity printHelloWorld()
     {
         return new ResponseEntity("Hello World", HttpStatus.OK);
+    }
+    @PostMapping("/encrypt")
+    public ResponseEntity<String> encryptString(@RequestBody EncryptReqDto encryptReqDto)
+    {
+        String result = cryptoService.encryptString(encryptReqDto.getPlainText());
+
+        if (result != null)
+        {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Something wicked this way comes", HttpStatus.OK);
+    }
+    @PostMapping("/decrypt")
+    public ResponseEntity<String> decryptString(@RequestBody DecryptReqDto decryptReqDto)
+    {
+        String result = cryptoService.decryptString(decryptReqDto.getCipherText());
+        if (result != null)
+        {
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Something wicked this way comes", HttpStatus.OK);
     }
 }
---
+++ src/test/java/com/cryptography/service/CryptoServiceTest.java
@@ -0,0 +1,24 @@
+package com.cryptography.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class CryptoServiceTest {
+    @Autowired
+    CryptoService cryptoService;
+
+    @Test
+    public void given_StringToBeEncrypted_ThenTestPasses()
+    {
+        assertThat(cryptoService.encryptString("parinaz")).isNotEqualTo(null);
+    }
+    @Test
+    public void given_CipherTextToBeDecrypted_ThenTestPasses()
+    {
+        assertThat(cryptoService.decryptString("0W2zO1PeZZuBG/W1VXFYb4f2LkyG+gh//pJj+N0eong=")).isNotEqualTo(null);
+    }
+}
