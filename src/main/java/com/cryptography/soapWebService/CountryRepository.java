61caf8bbe738b66e5c0091187a9ac38e1a26775c
parinaz <p.sattarzadeh@gmail.com>
2022-09-30 20:22:36 +0330

    implement a Soap WebService for Test

---
+++ src/main/java/com/cryptography/soapWebService/CountryRepository.java
@@ -0,0 +1,23 @@
+package com.cryptography.soapWebService;
+
+import com.cryptography.soapClasses.Country;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CountryRepository {
+
+    private static final Map<String, Country> countries = new HashMap<>();
+
+    @PostConstruct
+    public void initData() {
+        // initialize countries map
+    }
+
+    public Country findCountry(String name) {
+        return countries.get(name);
+    }
+}
